5893cb976337f5e4c064f5ea0304991cd159104a
WooJin Park(Ader) <ak2j38@gmail.com>
2024-02-11 00:59:26 +0900

    백준 - 평범한 배낭 풀이

--- src/main/kotlin/BOJ_12865.kt
+++ src/main/kotlin/BOJ_12865.kt
@@ -1,22 +1,35 @@
+import kotlin.math.max
+
+private var n: Int = 0
+private var k: Int = 0
+private var answer: Int = 0
+private var items: MutableList<Pair<Int, Int>> = mutableListOf()
+
+
 fun main() {
     val br = System.`in`.bufferedReader()
-    val (n, k) = br.readLine().split(" ").map { it.toInt() }
+    val (n1, k1) = br.readLine().split(" ").map { it.toInt() }
+    n = n1
+    k = k1
 
     for (i in 0 until n) {
         val (w, v) = br.readLine().split(" ").map { it.toInt() }
-
+        items.add(Pair(w, v))
     }
-    recur(0, 0, 0, k, n)
+    recur(0, 0, 0)
+
+    println(answer)
 }
 
-fun recur(index: Int, weight: Int, value: Int, maxWeight: Int, count: Int) {
-    if (weight > maxWeight) {
-
+fun recur(index: Int, weight: Int, value: Int) {
+    if (weight > k) {
+        return
     }
-    if (index == count) {
-
+    if (index == n) {
+        answer = max(answer, value)
+        return
     }
 
-    recur(index + 1, weight +, value +, maxWeight, count)
-    recur(index + 1, weight, value, maxWeight, count)
+    recur(index + 1, weight + items[index].first, value + items[index].second)
+    recur(index + 1, weight, value)
 }
